471931258a1deaae050f640e653faf351ba3695f
Kate Bryan <katebryan31@gmail.com>
2021-05-09 06:50:07 +0100

    feat: added cremaining functions and logic to minigame

--- dragon.py
+++ dragon.py
@@ -14,3 +14,31 @@ def chooseCave():
         cave = input()
 
     return cave
+
+
+def checkCave(chosenCave):
+    print('You approach the cave...')
+    time.sleep(2)
+    print('It is dark and spooky...')
+    time.sleep(2)
+    print('A large dragon jumps out in front of you! They open their jaws and...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+        print('Gives you their treasure!')
+    else:
+        print('Gobbles you down in one bite!')
+
+
+playAgain = 'yes'
+
+while playAgain == 'yes' or playAgain == 'y' or playAgain == 'Yes':
+    displayIntro()
+    caveNumber = chooseCave()
+    checkCave(caveNumber)
+
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
