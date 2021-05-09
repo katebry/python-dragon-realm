3da848c8e9cd175c0e005170c31a238214933674
Kate Bryan <katebryan31@gmail.com>
2021-05-09 06:39:29 +0100

    feat: beginning of dragoncave logic, 2 functions introduced

---
+++ dragon.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+
+def displayIntro():
+    print('''You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you. The other dragon is greedy and hungry and will eat you on sight.''')
+    print()
+
+
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Which cave will you go into? (1 or 2)')
+        cave = input()
+
+    return cave
